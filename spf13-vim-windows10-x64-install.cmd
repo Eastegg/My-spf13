7371c182ec41ae012e857f3900b2b01a17b6be52
Ed <tdbsdd@gmail.com>
2016-12-16 10:53:21 +0800

    auto open list tree

--- spf13-vim-windows10-x64-install.cmd
+++ spf13-vim-windows10-x64-install.cmd
@@ -1,16 +1,16 @@
-REM    Copyright 2014 Steve Francia
-REM 
-REM    Licensed under the Apache License, Version 2.0 (the "License");
-REM    you may not use this file except in compliance with the License.
-REM    You may obtain a copy of the License at
-REM 
-REM        http://www.apache.org/licenses/LICENSE-2.0
-REM 
-REM    Unless required by applicable law or agreed to in writing, software
-REM    distributed under the License is distributed on an "AS IS" BASIS,
-REM    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM    See the License for the specific language governing permissions and
-REM    limitations under the License.
+@REM    Copyright 2014 Steve Francia
+@REM
+@REM    Licensed under the Apache License, Version 2.0 (the "License");
+@REM    you may not use this file except in compliance with the License.
+@REM    You may obtain a copy of the License at
+@REM
+@REM        http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM    Unless required by applicable law or agreed to in writing, software
+@REM    distributed under the License is distributed on an "AS IS" BASIS,
+@REM    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM    See the License for the specific language governing permissions and
+@REM    limitations under the License.
 
 
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
@@ -20,7 +20,7 @@ REM    limitations under the License.
 IF NOT EXIST "%APP_PATH%" (
     call git clone -b 3.0 https://github.com/spf13/spf13-vim.git "%APP_PATH%"
 ) ELSE (
-    @set ORIGINAL_DIR=%CD%
+    @set ORIGINAL_DIR='%CD%'
     echo updating spf13-vim
     chdir /d "%APP_PATH%"
     call git pull
@@ -36,10 +36,6 @@ call mklink "%HOME%\.vimrc.bundles.fork" "%APP_PATH%\.vimrc.bundles.fork"
 call mklink "%HOME%\.vimrc.before" "%APP_PATH%\.vimrc.before"
 call mklink "%HOME%\.vimrc.before.fork" "%APP_PATH%\.vimrc.before.fork"
 call mklink /J "%HOME%\.vim" "%APP_PATH%\.vim"
-call mklink "%HOME%\.vimrc.local" "%APP_PATH%\.vimrc.local"
-call mklink "%HOME%\.gvimrc.local" "%APP_PATH%\.gvimrc.local"
-call mklink "%HOME%\.vimrc.bundles.local" "%APP_PATH%\.vimrc.bundles.local"
-
 
 IF NOT EXIST "%APP_PATH%\.vim\bundle" (
     call mkdir "%APP_PATH%\.vim\bundle"
